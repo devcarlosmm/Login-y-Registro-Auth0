790aa40026c783b7791665dc5b805cb0e2e2ba8b
Carlos Morillo moreno <carlosmorillomoreno@MacBook-Air-de-Carlos.local>
2021-10-13 12:15:34 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+
+// Auth0 
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain:'dev-1lhxbuj0.us.auth0.com',
+      clientId:'0bWWqGz26V1RrUx2AJhZLPM8wwPCZirm'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
